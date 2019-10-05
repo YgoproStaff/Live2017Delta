c7b3c5505cebb08b388cde451fd580d745435367
HPF Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2019-10-04 20:40:07 -0400

    b37639a0f97da3021d4c8ba4cd9b4af952e70098 40316ab9de57e2bbfe4889d92018cc4e62318f37

--- c84117021.lua
+++ c84117021.lua
@@ -1,21 +1,23 @@
 --魔力隔壁
-function c84117021.initial_effect(c)
+--Spell Wall
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c84117021.target)
-	e1:SetOperation(c84117021.activate)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function c84117021.filter(c)
+function s.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_SPELLCASTER)
 end
-function c84117021.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local opt=0
-	if Duel.IsExistingTarget(c84117021.filter,tp,LOCATION_MZONE,0,1,nil) then
-		opt=Duel.SelectOption(tp,aux.Stringid(84117021,0),aux.Stringid(84117021,1))
+	if Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) then
+		opt=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
 	end
 	Duel.SetTargetParam(opt)
 	if opt==0 then
@@ -23,10 +25,10 @@ function c84117021.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	else
 		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-		Duel.SelectTarget(tp,c84117021.filter,tp,LOCATION_MZONE,0,1,1,nil)
+		Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	end
 end
-function c84117021.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local opt=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	if opt==0 then
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -45,8 +47,8 @@ function c84117021.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e4=Effect.CreateEffect(e:GetHandler())
 		e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e4:SetCode(EVENT_SUMMON_SUCCESS)
-		e4:SetCondition(c84117021.sumcon)
-		e4:SetOperation(c84117021.sumsuc)
+		e4:SetCondition(s.sumcon)
+		e4:SetOperation(s.sumsuc)
 		e4:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e4,tp)
 		local e5=e4:Clone()
@@ -55,14 +57,20 @@ function c84117021.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e6=e4:Clone()
 		e6:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 		Duel.RegisterEffect(e6,tp)
+		local e7=Effect.CreateEffect(e:GetHandler())
+		e7:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e7:SetCode(EVENT_CHAIN_END)
+		e7:SetOperation(s.cedop)
+		e7:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e7,tp)
 	else
 		local tc=Duel.GetFirstTarget()
 		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 			e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-			e1:SetOperation(c84117021.atkop)
-			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+			e1:SetOperation(s.atkop)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			tc:RegisterEffect(e1)
 		end
 	end
@@ -81,16 +89,22 @@ function c84117021.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.RegisterEffect(e2,tp)
 	end
 end
-function c84117021.sumcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c84117021.filter,1,nil)
+function s.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil)
 end
-function c84117021.sumsuc(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(c84117021.efun)
+function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(s.chlimit)
 end
-function c84117021.efun(e,ep,tp)
-	return ep==tp
+function s.cedop(e,tp,eg,ep,ev,re,r,rp)
+	local _,g=Duel.CheckEvent(EVENT_SPSUMMON_SUCCESS,true)
+	if g and g:IsExists(s.filter,1,nil) and Duel.CheckEvent(EVENT_SPSUMMON_SUCCESS) then
+		Duel.SetChainLimitTillChainEnd(s.chlimit)
+	end
 end
-function c84117021.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.chlimit(re,rp,tp)
+	return rp==tp
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
