687beb96d3f72ea0dfb698b58a030419a42065f8
HPF Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2019-11-02 11:15:10 -0400

    35d0552499bc33c12d781017bfe3d3559a87e8f9 e49875700fc7cbb0818666e5cccf5b3118165036

--- c511009105.lua
+++ c511009105.lua
@@ -1,5 +1,7 @@
---Performage Trapeze Force Witch
-function c511009105.initial_effect(c)
+--Ｅｍトラピーズ・フォーズ・ウィッチ
+--Performage Trapeze Witch
+local s,id=GetID()
+function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcMixN(c,true,true,aux.FilterBoolFunction(Card.IsFusionSetCard,0xc6),2)
@@ -9,7 +11,7 @@ function c511009105.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(c511009105.target)
+	e2:SetTarget(s.target)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
 	--cannot be target
@@ -17,11 +19,11 @@ function c511009105.initial_effect(c)
 	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e3:SetValue(aux.tgoval)
 	c:RegisterEffect(e3)
-	
+	--cannot be effect target
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
-	e4:SetCondition(c511009105.atcon)
+	e4:SetCondition(s.atcon)
 	e4:SetValue(1)
 	c:RegisterEffect(e4)
 	--atk
@@ -31,44 +33,45 @@ function c511009105.initial_effect(c)
 	e5:SetCode(EVENT_BATTLE_START)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e5:SetCondition(c511009105.atkcon)
-	e5:SetTarget(c511009105.atktg)
-	e5:SetOperation(c511009105.atkop)
+	e5:SetCondition(s.atkcon)
+	e5:SetTarget(s.atktg)
+	e5:SetOperation(s.atkop)
 	c:RegisterEffect(e5)
 end
-c511009105.material_setcode=0xc6
-function c511009105.target(e,c)
+s.material_setcode=0xc6
+function s.target(e,c)
 	return c:IsSetCard(0xc6)
 end
-function c511009105.atfilter(c)
+function s.atfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0xc6)
 end
-function c511009105.atcon(e)
-	return Duel.IsExistingMatchingCard(c511009105.atfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
+function s.atcon(e)
+	return Duel.IsExistingMatchingCard(s.atfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
 end
-function c511009105.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	local a=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
-	if d and a:GetControler()~=d:GetControler() then
-		if a:IsControler(tp) and a:IsSetCard(0xc6) then e:SetLabelObject(d)
-		elseif (d:IsSetCard(0xc6)) then e:SetLabelObject(a) end
-		return true
-	else return false end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	local bc=Duel.GetAttackTarget()
+	if not bc then return false end
+	if bc:IsControler(1-tp) then bc=tc end
+	e:SetLabelObject(bc:GetBattleTarget())
+	return bc:IsFaceup() and bc:IsSetCard(0xc6)
 end
-function c511009105.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local tc=e:GetLabelObject()
 	if chkc then return chkc==tc end
-	if chk==0 then return tc:IsOnField() and tc:IsFaceup() and tc:IsCanBeEffectTarget(e) end
+	if chk==0 then
+		return tc:IsOnField() and tc:IsFaceup() and tc:IsCanBeEffectTarget(e)
+	end
 	Duel.SetTargetCard(tc)
 end
-function c511009105.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(-600)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
 end
