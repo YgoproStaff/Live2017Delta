b9f4f686b23b2c7887434eb17318894f24a53716
HPF Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2019-11-04 06:00:09 -0500

    04fc25bff51d732095f3b4eea19447dc277a313e e49875700fc7cbb0818666e5cccf5b3118165036

--- c47355498.lua
+++ c47355498.lua
@@ -1,5 +1,7 @@
 --王家の眠る谷－ネクロバレー
-function c47355498.initial_effect(c)
+--Necrovalley
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -23,11 +25,11 @@ function c47355498.initial_effect(c)
 	e4:SetCode(EFFECT_CANNOT_REMOVE)
 	e4:SetRange(LOCATION_FZONE)
 	e4:SetTargetRange(LOCATION_GRAVE,0)
-	e4:SetCondition(c47355498.contp)
+	e4:SetCondition(s.contp)
 	c:RegisterEffect(e4)
 	local e5=e4:Clone()
 	e5:SetTargetRange(0,LOCATION_GRAVE)
-	e5:SetCondition(c47355498.conntp)
+	e5:SetCondition(s.conntp)
 	c:RegisterEffect(e5)
 	--necro valley
 	local e6=Effect.CreateEffect(c)
@@ -35,11 +37,11 @@ function c47355498.initial_effect(c)
 	e6:SetCode(EFFECT_NECRO_VALLEY)
 	e6:SetRange(LOCATION_FZONE)
 	e6:SetTargetRange(LOCATION_GRAVE,0)
-	e6:SetCondition(c47355498.contp)
+	e6:SetCondition(s.contp)
 	c:RegisterEffect(e6)
 	local e7=e6:Clone()
 	e7:SetTargetRange(0,LOCATION_GRAVE)
-	e7:SetCondition(c47355498.conntp)
+	e7:SetCondition(s.conntp)
 	c:RegisterEffect(e7)
 	local e8=Effect.CreateEffect(c)
 	e8:SetType(EFFECT_TYPE_FIELD)
@@ -47,31 +49,31 @@ function c47355498.initial_effect(c)
 	e8:SetRange(LOCATION_FZONE)
 	e8:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e8:SetTargetRange(1,0)
-	e8:SetCondition(c47355498.contp)
+	e8:SetCondition(s.contp)
 	c:RegisterEffect(e8)
 	local e9=e8:Clone()
 	e9:SetTargetRange(0,1)
-	e9:SetCondition(c47355498.conntp)
+	e9:SetCondition(s.conntp)
 	c:RegisterEffect(e9)
 	--disable
 	local e10=Effect.CreateEffect(c)
 	e10:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e10:SetCode(EVENT_CHAIN_SOLVING)
 	e10:SetRange(LOCATION_FZONE)
-	e10:SetOperation(c47355498.disop)
+	e10:SetOperation(s.disop)
 	c:RegisterEffect(e10)
 end
-function c47355498.contp(e)
+function s.contp(e)
 	return not Duel.IsPlayerAffectedByEffect(e:GetHandler():GetControler(),EFFECT_NECRO_VALLEY_IM)
 end
-function c47355498.conntp(e)
+function s.conntp(e)
 	return not Duel.IsPlayerAffectedByEffect(1-e:GetHandler():GetControler(),EFFECT_NECRO_VALLEY_IM)
 end
-function c47355498.disfilter(c,im0,im1)
-	if c:IsControler(0) then return im0 and c:IsHasEffect(EFFECT_NECRO_VALLEY)
-	else return im1 and c:IsHasEffect(EFFECT_NECRO_VALLEY) end
+function s.disfilter(c,im0,im1,re)
+	if c:IsControler(0) then return im0 and c:IsHasEffect(EFFECT_NECRO_VALLEY) and c:IsRelateToEffect(re)
+	else return im1 and c:IsHasEffect(EFFECT_NECRO_VALLEY) and c:IsRelateToEffect(re) end
 end
-function c47355498.discheck(ev,category,re,im0,im1)
+function s.discheck(ev,category,re,im0,im1)
 	local ex,tg,ct,p,v=Duel.GetOperationInfo(ev,category)
 	if not ex then return false end
 	if v==LOCATION_GRAVE then
@@ -80,21 +82,21 @@ function c47355498.discheck(ev,category,re,im0,im1)
 		elseif p==PLAYER_ALL then return im0 and im1
 		end
 	end
-	if tg and tg:GetCount()>0 then
-		return tg:IsExists(c47355498.disfilter,1,nil,im0,im1)
+	if tg and #tg>0 then
+		return tg:IsExists(s.disfilter,1,nil,im0,im1,re)
 	end
 	return false
 end
-function c47355498.disop(e,tp,eg,ep,ev,re,r,rp)
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=re:GetHandler()
 	if not Duel.IsChainDisablable(ev) or tc:IsHasEffect(EFFECT_NECRO_VALLEY_IM) then return end
 	local res=false
 	local im0=not Duel.IsPlayerAffectedByEffect(0,EFFECT_NECRO_VALLEY_IM)
 	local im1=not Duel.IsPlayerAffectedByEffect(1,EFFECT_NECRO_VALLEY_IM)
-	if not res and c47355498.discheck(ev,CATEGORY_SPECIAL_SUMMON,re,im0,im1) then res=true end
-	if not res and c47355498.discheck(ev,CATEGORY_REMOVE,re,im0,im1) then res=true end
-	if not res and c47355498.discheck(ev,CATEGORY_TOHAND,re,im0,im1) then res=true end
-	if not res and c47355498.discheck(ev,CATEGORY_TODECK,re,im0,im1) then res=true end
-	if not res and c47355498.discheck(ev,CATEGORY_LEAVE_GRAVE,re,im0,im1) then res=true end
+	if not res and s.discheck(ev,CATEGORY_SPECIAL_SUMMON,re,im0,im1) then res=true end
+	if not res and s.discheck(ev,CATEGORY_REMOVE,re,im0,im1) then res=true end
+	if not res and s.discheck(ev,CATEGORY_TOHAND,re,im0,im1) then res=true end
+	if not res and s.discheck(ev,CATEGORY_TODECK,re,im0,im1) then res=true end
+	if not res and s.discheck(ev,CATEGORY_LEAVE_GRAVE,re,im0,im1) then res=true end
 	if res then Duel.NegateEffect(ev) end
 end
