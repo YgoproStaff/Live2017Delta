f3a10e32f2c80f2afe1d9564d061348fb83ec88b
HPF Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2019-11-16 02:05:10 -0500

    9cdc7c71ecf58d36babf686d0041ba8ab09ec74a f1aae03a7a6253987026335cf256565b30249b7a

--- c100000412.lua
+++ c100000412.lua
@@ -1,5 +1,8 @@
---白薔薇の回廊
-function c100000412.initial_effect(c)
+--白薔薇の回廊 (Manga)
+--White Rose Cloister (Manga)
+local s,id,alias=GetID()
+function s.initial_effect(c)
+	alias=c:GetOriginalCodeRule()
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -7,30 +10,30 @@ function c100000412.initial_effect(c)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100000412,0))
+	e2:SetDescription(aux.Stringid(alias,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetCountLimit(1)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTarget(c100000412.target)
-	e2:SetOperation(c100000412.operation)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
-	aux.CallToken(420)
 end
-function c100000412.filter(c,e,sp)
-	return c:IsRose() and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
+s.listed_series={0x123}
+function s.filter(c,e,sp)
+	return c:IsSetCard(0x123) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
 end
-function c100000412.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and Duel.IsExistingMatchingCard(c100000412.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c100000412.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c100000412.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	if g:GetCount()>0 then
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetFirst() then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
--- c511002216.lua
+++ c511002216.lua
@@ -1,5 +1,7 @@
+--黑薔薇の種
 --Black Rose Seed
-function c511002216.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--atkdown
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -7,39 +9,39 @@ function c511002216.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_DAMAGE_STEP)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetCondition(c511002216.condition)
-	e1:SetTarget(c511002216.target)
-	e1:SetOperation(c511002216.activate)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	aux.CallToken(420)
 end
-function c511002216.condition(e,tp,eg,ep,ev,re,r,rp)
+s.listed_series={0x123}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local phase=Duel.GetCurrentPhase()
 	if phase~=PHASE_DAMAGE or Duel.IsDamageCalculated() then return false end
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
-	if a:IsRose() and (not d or not d:IsRose()) and a:IsRelateToBattle() then
+	if a:IsSetCard(0x123) and (not d or not d:IsSetCard(0x123)) and a:IsRelateToBattle() then
 		e:SetLabelObject(a)
 		return true
 	end
-	if not a:IsRose() and d and d:IsRose() and d:IsRelateToBattle() then
+	if not a:IsSetCard(0x123) and d and d:IsSetCard(0x123) and d:IsRelateToBattle() then
 		e:SetLabelObject(d)
 		return true
 	end
 	return false
 end
-function c511002216.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=e:GetLabelObject()
 	if chk==0 then return true end
 	Duel.SetTargetCard(tc)
 end
-function c511002216.activate(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.activate(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=Duel.GetFirstTarget()
 	if not tc:IsRelateToBattle() then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetReset(RESET_EVENT+0x1fe0000)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	e1:SetValue(800)
 	tc:RegisterEffect(e1)
 	local e2=e1:Clone()
