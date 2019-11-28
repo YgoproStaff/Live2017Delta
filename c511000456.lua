5c32f8eb69c3520695cf9595b4cbaac3e38088b4
HPF Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2019-11-28 11:05:12 -0500

    d6fe2bbee8c64070200cc61d95a545a3dfa46f70 7825c790edb2945dcd30ed5526af0ce716655d48

--- c511000456.lua
+++ c511000456.lua
@@ -1,25 +1,27 @@
 --Wish of Final Effort
-function c511000456.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_BATTLE_DESTROYED)
-	e1:SetTarget(c511000456.target)
-	e1:SetOperation(c511000456.activate)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function c511000456.filter(c,tp)
-	return c:IsLocation(LOCATION_GRAVE) and c:GetPreviousControler()==tp and c:IsReason(REASON_BATTLE) and c:GetAttack()>0
+function s.filter(c,tp)
+	return c:GetAttack()>0 and c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousControler()==tp and c:GetControler()==tp
 end
-function c511000456.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return eg:IsContains(chkc,tp) and c511000456.filter(chkc,tp) end
-	if chk==0 then return eg:IsExists(c511000456.filter,1,nil,tp) end
-	local g=eg:Filter(c511000456.filter,nil,tp)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return eg:IsContains(chkc,tp) and s.filter(chkc,tp) end
+	if chk==0 then return eg:IsExists(s.filter,1,nil,tp) end
+	local g=eg:Filter(s.filter,nil,tp)
 	e:SetLabelObject(g:GetFirst())
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,g:GetFirst():GetAttack())
 end
-function c511000456.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local atk=e:GetLabelObject():GetAttack()
 	Duel.Recover(tp,atk,REASON_EFFECT)
 end
