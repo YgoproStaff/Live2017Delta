69caa77a4db47b41d9f5dbb5e0e6bc3fa8ea917a
HPF Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2019-12-01 08:05:17 -0500

    04ae1d3c14f9f8f2d4c04efdd2b679085b8ba42a 7825c790edb2945dcd30ed5526af0ce716655d48

--- c95526884.lua
+++ c95526884.lua
@@ -1,5 +1,7 @@
 --ハイパーサイコガンナー
-function c95526884.initial_effect(c)
+--Hyper Psychic Blaster
+local s,id=GetID()
+function s.initial_effect(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,nil,1,1,aux.NonTunerEx(Card.IsRace,RACE_PSYCHO),1,99)
 	c:EnableReviveLimit()
@@ -10,32 +12,32 @@ function c95526884.initial_effect(c)
 	c:RegisterEffect(e1)
 	--recover
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(95526884,0))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_RECOVER)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_DAMAGE_STEP_END)
-	e2:SetCondition(c95526884.reccon)
-	e2:SetTarget(c95526884.rectg)
-	e2:SetOperation(c95526884.recop)
+	e2:SetCondition(s.reccon)
+	e2:SetTarget(s.rectg)
+	e2:SetOperation(s.recop)
 	c:RegisterEffect(e2)
 end
-function c95526884.reccon(e,tp,eg,ep,ev,re,r,rp)
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
 	if not d then return false end
-	local m=a:GetAttack()-d:GetDefense()
+	local m=a:GetAttack()-d:GetPreviousDefenseOnField()
 	e:SetLabel(m)
-	return c==a and d:GetDefense()>=0 and m>0 and bit.band(d:GetBattlePosition(),POS_DEFENSE)~=0
+	return c==a and d:GetPreviousDefenseOnField()>=0 and m>0 and (d:GetBattlePosition()&POS_DEFENSE)~=0
 end
-function c95526884.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(e:GetLabel())
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,e:GetLabel())
 end
-function c95526884.recop(e,tp,eg,ep,ev,re,r,rp)
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Recover(p,d,REASON_EFFECT)
 end
