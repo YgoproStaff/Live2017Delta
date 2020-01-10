33c60b517bdab239c0ab6d05395c1502d2f6a90e
HPF Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2020-01-10 18:40:07 -0500

    3700239928b74ac4cbe2025ad36ddb22519f7fdf 501d8c27b9917084fa6b10b891ca079ee3dc0363

--- c511003025.lua
+++ c511003025.lua
@@ -1,28 +1,30 @@
---コピー・プラント
-function c511003025.initial_effect(c)
+--コピー・プラント (Anime)
+--Copy Plant (Anime)
+local s,id,alias=GetID()
+function s.initial_effect(c)
+	alias=c:GetOriginalCodeRule()
 	--lvchange
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(66457407,0))
+	e1:SetDescription(aux.Stringid(alias,0))
 	e1:SetCategory(CATEGORY_LVCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(c511003025.lvtg)
-	e1:SetOperation(c511003025.lvop)
+	e1:SetTarget(s.lvtg)
+	e1:SetOperation(s.lvop)
 	c:RegisterEffect(e1)
 end
-function c511003025.lvfilter(c,lv)
-	return c:IsFaceup() and c:GetLevel()>0 and c:GetLevel()~=lv
+function s.lvfilter(c,lv)
+	return c:IsFaceup() and c:GetLevel()>0
 end
-function c511003025.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local lv=e:GetHandler():GetLevel()
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c511003025.lvfilter(chkc,lv) end
-	if chk==0 then return Duel.IsExistingTarget(c511003025.lvfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,lv) end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.lvfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c511003025.lvfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,lv)
+	Duel.SelectTarget(tp,s.lvfilter,tp,0,LOCATION_MZONE,1,1,nil)
 end
-function c511003025.lvop(e,tp,eg,ep,ev,re,r,rp)
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsFaceup() and c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
@@ -30,7 +32,7 @@ function c511003025.lvop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
 		e1:SetValue(tc:GetLevel())
-		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
 	end
 end
