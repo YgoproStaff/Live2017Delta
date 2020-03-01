b5a494f1bd1bb082be1954afbb4b4637aed97ebd
Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2020-03-01 06:10:37 -0500

    234ab8c5147842eb16c599b15166be009201b546 33ce00c946caa2fcb17a9c892dd4369778d191b5

--- c53569894.lua
+++ c53569894.lua
@@ -1,5 +1,7 @@
 --光のピラミッド
-function c53569894.initial_effect(c)
+--Pyramid of Light
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -10,28 +12,28 @@ function c53569894.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e2:SetCode(EVENT_LEAVE_FIELD_P)
-	e2:SetOperation(c53569894.checkop)
+	e2:SetOperation(s.checkop)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_LEAVE_FIELD)
 	e3:SetLabelObject(e2)
-	e3:SetOperation(c53569894.leave)
+	e3:SetOperation(s.leave)
 	c:RegisterEffect(e3)
 end
-function c53569894.filter(c)
+function s.filter(c)
 	return c:IsFaceup() and c:IsCode(15013468,51402177)
 end
-function c53569894.checkop(e,tp,eg,ep,ev,re,r,rp)
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsDisabled() then
 		e:SetLabel(1)
 	else e:SetLabel(0) end
 end
-function c53569894.leave(e,tp,eg,ep,ev,re,r,rp)
+function s.leave(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if e:GetLabelObject():GetLabel()==0 and c:GetPreviousControler()==tp and c:IsStatus(STATUS_ACTIVATED) then
-		local g=Duel.GetMatchingGroup(c53569894.filter,tp,LOCATION_ONFIELD,0,nil)
+	if e:GetLabelObject():GetLabel()==0 and c:GetPreviousControler()==tp and c:GetPreviousPosition(POS_FACEUP) then
+		local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,0,nil)
 		Duel.Destroy(g,REASON_EFFECT,LOCATION_REMOVED)
 	end
 end
