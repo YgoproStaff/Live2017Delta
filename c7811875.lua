67c9ffb9e2c50f6698e8c6a3f91443c18fbdc481
Cron Assembler <49610416+YgoproStaff@users.noreply.github.com>
2020-03-26 16:50:08 -0400

    fd357c075e1eee09ed83a49ef150a1e0cdbab78d 7c2c70b09c92a0032adcd3754e5228e9484ba67f

--- c7811875.lua
+++ c7811875.lua
@@ -1,14 +1,16 @@
 --重力崩壊
-function c7811875.initial_effect(c)
+--Gravity Collapse
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate(summon)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DISABLE_SUMMON+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SUMMON)
-	e1:SetCondition(c7811875.condition)
-	e1:SetCost(c7811875.cost)
-	e1:SetTarget(c7811875.target)
-	e1:SetOperation(c7811875.activate)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_FLIP_SUMMON)
@@ -17,28 +19,26 @@ function c7811875.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON)
 	c:RegisterEffect(e3)
 end
-function c7811875.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentChain()==0 and eg:IsExists(Card.IsControler,1,nil,1-tp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain()==0 and ep==1-tp --tp~=ep
 end
-function c7811875.cfilter(c)
+function s.cfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsAbleToGraveAsCost()
 end
-function c7811875.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c7811875.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c7811875.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SendtoGrave(g,REASON_COST)
 end
-function c7811875.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=eg:Filter(Card.IsControler,nil,1-tp)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,g,g:GetCount(),0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,eg,#eg,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,#eg,0,0)
 end
-function c7811875.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(Card.IsControler,nil,1-tp)
-	Duel.NegateSummon(g)
-	Duel.Destroy(g,REASON_EFFECT)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateSummon(eg)
+    Duel.Destroy(eg,REASON_EFFECT)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
